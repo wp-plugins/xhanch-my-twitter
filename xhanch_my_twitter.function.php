c7e17d7da6e541f8fde65c4e95ff6638e13bdbb6
xhanch_studio <xhanch_studio@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-04 13:19:00 +0000

    git-svn-id: https://plugins.svn.wordpress.org/xhanch-my-twitter/trunk@212951 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- xhanch_my_twitter.function.php
+++ xhanch_my_twitter.function.php
@@ -1,5 +1,5 @@
 <?php
-	function my_twitter_log($str){
+	function xhanch_my_twitter_log($str){
 		$log_file = dirname(__FILE__).'/log.log';
 		$fp = @fopen($log_file, 'a+');
 		if($fp){
@@ -8,7 +8,7 @@
 		}
 	}
 
-	function my_twitter_make_url_clickable_cb($matches) {
+	function xhanch_my_twitter_make_url_clickable_cb($matches) {
 		$url = $matches[2];
 		$url = esc_url($url);
 		if ( empty($url) )
@@ -16,7 +16,7 @@
 		return $matches[1] . "<a href=\"$url\" rel=\"nofollow\" target=\"_blank\">$url</a>";
 	}
 
-	function my_twitter_make_web_ftp_clickable_cb($matches) {
+	function xhanch_my_twitter_make_web_ftp_clickable_cb($matches) {
 		$ret = '';
 		$dest = $matches[2];
 		$dest = 'http://' . $dest;
@@ -31,24 +31,24 @@
 		return $matches[1] . "<a href=\"$dest\" rel=\"nofollow\" target=\"_blank\">$dest</a>" . $ret;
 	}
 
-	function my_twitter_make_email_clickable_cb($matches) {
+	function xhanch_my_twitter_make_email_clickable_cb($matches) {
 		$email = $matches[2] . '@'.$matches[3];
 		return $matches[1] . "<a href=\"mailto:$email\" target=\"_blank\">$email</a>";
 	}
 
-	function my_twitter_make_clickable($ret) {
+	function xhanch_my_twitter_make_clickable($ret) {
 		$ret = ' ' . $ret;
 		// in testing, using arrays here was found to be faster
-		$ret = preg_replace_callback('#(?<=[\s>])(\()?([\w]+?://(?:[\w\\x80-\\xff\#$%&~/\-=?@\[\](+]|[.,;:](?![\s<])|(?(1)\)(?![\s<])|\)))+)#is', 'my_twitter_make_url_clickable_cb', $ret);
-		$ret = preg_replace_callback('#([\s>])((www|ftp)\.[\w\\x80-\\xff\#$%&~/.\-;:=,?@\[\]+]+)#is', 'my_twitter_make_web_ftp_clickable_cb', $ret);
-		//$ret = preg_replace_callback('#([\s>])([.0-9a-z_+-]+)@(([0-9a-z-]+\.)+[0-9a-z]{2,})#i', 'my_twitter_make_email_clickable_cb', $ret);
+		$ret = preg_replace_callback('#(?<=[\s>])(\()?([\w]+?://(?:[\w\\x80-\\xff\#$%&~/\-=?@\[\](+]|[.,;:](?![\s<])|(?(1)\)(?![\s<])|\)))+)#is', 'xhanch_my_twitter_make_url_clickable_cb', $ret);
+		$ret = preg_replace_callback('#([\s>])((www|ftp)\.[\w\\x80-\\xff\#$%&~/.\-;:=,?@\[\]+]+)#is', 'xhanch_my_twitter_make_web_ftp_clickable_cb', $ret);
+		//$ret = preg_replace_callback('#([\s>])([.0-9a-z_+-]+)@(([0-9a-z-]+\.)+[0-9a-z]{2,})#i', 'xhanch_my_twitter_make_email_clickable_cb', $ret);
 		// this one is not in an array because we need it to run last, for cleanup of accidental links within links
 		$ret = preg_replace("#(<a( [^>]+?>|>))<a [^>]+?>([^>]+?)</a></a>#i", "$1$3</a>", $ret);
 		$ret = trim($ret);
 		return $ret;
 	}
 
-	function my_twitter_time_span($unix_date){	 
+	function xhanch_my_twitter_time_span($unix_date){	 
 		$periods = array("second", "minute", "hour", "day", "week", "month", "year", "decade");
 		$lengths = array("60","60","24","7","4.35","12","10");
 	 
@@ -76,13 +76,13 @@
 		return "$difference $periods[$j] {$tense}";
 	}
 
-	function my_twitter_form_get($str){
+	function xhanch_my_twitter_form_get($str){
 		if(!isset($_GET[$str]))
 			return false;
 		return urldecode($_GET[$str]);
 	}
 
-	function my_twitter_read_var($str){
+	function xhanch_my_twitter_read_var($str){
 		$res = $str;
 		$res = str_replace('\\\'','\'',$res);
 		$res = str_replace('\\\\','\\',$res);
@@ -90,15 +90,15 @@
 		return $res;
 	}
 
-	function my_twitter_form_post($str, $parse = true){
+	function xhanch_my_twitter_form_post($str, $parse = true){
 		if(!isset($_POST[$str]))
 			return false;
 		if($parse)
-			return my_twitter_read_var($_POST[$str]);
+			return xhanch_my_twitter_read_var($_POST[$str]);
 		return $_POST[$str];
 	}
 
-	function my_twitter_get_dir($type) {
+	function xhanch_my_twitter_get_dir($type) {
 		if ( !defined('WP_CONTENT_URL') )
 			define( 'WP_CONTENT_URL', get_option('siteurl') . '/wp-content');
 		if ( !defined('WP_CONTENT_DIR') )
@@ -107,7 +107,7 @@
 		else { return WP_CONTENT_URL.'/plugins/'.plugin_basename(dirname(__FILE__)); }
 	}
 
-	function my_twitter_get_file($name){
+	function xhanch_my_twitter_get_file($name){
 		$res = '';
 		$res = @file_get_contents($name);
 		if($res === false || $res == ''){
@@ -121,13 +121,13 @@
 			$res = curl_exec($ch);
 
 			if(!$res)
-				my_twitter_log('Failed to read feeds from twitter');
+				xhanch_my_twitter_log('Failed to read feeds from twitter');
 			curl_close($ch);
 		}
 		return $res;
 	}	
 
-	function my_twitter_get_time($dt){
+	function xhanch_my_twitter_get_time($dt){
 		//Tue Feb 16 23:41:29 +0000 2010 
 		$tmp = explode(' ', $dt);
 		$time = explode(':', $tmp[3]);
@@ -149,29 +149,29 @@
 		return mktime($time[0], $time[1], $time[2], $tmp[1], $tmp[2], $tmp[5]) + $gmt_add;
 	}
 
-	function my_twitter_get_tweets(){
-		$uid = get_option('my_twitter_id');
-		$limit = intval(get_option('my_twitter_count'));
-		$show_post_by = intval(get_option('my_twitter_show_post_by'));
+	function xhanch_my_twitter_get_tweets(){
+		$uid = get_option('xhanch_my_twitter_id');
+		$limit = intval(get_option('xhanch_my_twitter_count'));
+		$show_post_by = intval(get_option('xhanch_my_twitter_show_post_by'));
 
 		if($limit <= 0)
 			$limit = 5;
 
 		$api_url = 'http://twitter.com/statuses/user_timeline/'.urlencode($uid).'.xml?count='.$limit;
 
-		$req = my_twitter_get_file($api_url); 
-			my_twitter_log($req);
+		$req = xhanch_my_twitter_get_file($api_url); 
+			xhanch_my_twitter_log($req);
 		if($req == ''){
-			my_twitter_log('Feed retrieved from twitter is empty');
+			xhanch_my_twitter_log('Feed retrieved from twitter is empty');
 			return array();
 		}
 		
 		$xml = @new SimpleXMLElement($req);
 
 		if(!$xml)
-			my_twitter_log('Failed to parse feeds retrieved from twitter');			
+			xhanch_my_twitter_log('Failed to parse feeds retrieved from twitter');			
 		if(!isset($xml->entry))
-			my_twitter_log('Feeds retrieved from twitter is not well formed');		
+			xhanch_my_twitter_log('Feeds retrieved from twitter is not well formed');		
 		
 		$items_count= count($xml->entry);
 		if($items_count < $limit)
@@ -186,18 +186,18 @@
 			$date_time = (string)$res->created_at;
 			
 			$timestamp = '';
-			$date_format = get_option('my_twitter_date_format');
+			$date_format = get_option('xhanch_my_twitter_date_format');
 			if($date_format != ''){
 				$timestamp = ' - posted ';
 				if($date_format == 'span')
-					$timestamp .= my_twitter_time_span(my_twitter_get_time($date_time));
+					$timestamp .= xhanch_my_twitter_time_span(xhanch_my_twitter_get_time($date_time));
 				else
-					$timestamp .= ' on '.date($date_format, my_twitter_get_time($date_time));
+					$timestamp .= ' on '.date($date_format, xhanch_my_twitter_get_time($date_time));
 			}
 			
 			$pattern = '/\@([a-zA-Z]+)/';
 			$replace = '<a href="http://twitter.com/'.strtolower('\1').'">@\1</a>';
-			$output = convert_smilies(preg_replace($pattern,$replace,my_twitter_make_clickable($res->text)));
+			$output = convert_smilies(preg_replace($pattern,$replace,xhanch_my_twitter_make_clickable($res->text)));
 
 			$author_name = (string)$res->user->name;
 			$author_uid = (string)$res->user->screen_name;
@@ -216,14 +216,14 @@
 
 		if($show_post_by != 'hidden_personal'){
 			$api_url_reply = 'http://search.twitter.com/search.atom?q=to:'.urlencode($uid);
-			$req = my_twitter_get_file($api_url_reply); 
+			$req = xhanch_my_twitter_get_file($api_url_reply); 
 			if($req == '')
 				return array();
 
 			$xml = @new SimpleXMLElement($req);		
 			$items_count = count($xml->entry);
 			
-			$limit = intval(get_option('my_twitter_count'));
+			$limit = intval(get_option('xhanch_my_twitter_count'));
 			if($items_count < $limit)
 				$limit = $items_count;
 
@@ -262,7 +262,7 @@
 			}
 			krsort($arr);
 
-			$limit = intval(get_option('my_twitter_count'));
+			$limit = intval(get_option('xhanch_my_twitter_count'));
 			if(count($arr) > $limit){
 				do{
 					array_pop($arr);
