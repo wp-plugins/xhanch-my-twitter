cc4121609eda8730e64ece99175eeaada7c5c3e8
xhanch_studio <xhanch_studio@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-04-26 12:30:26 +0000

    git-svn-id: https://plugins.svn.wordpress.org/xhanch-my-twitter/trunk@233955 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- xhanch_my_twitter.php
+++ xhanch_my_twitter.php
@@ -5,7 +5,7 @@
 		Description: Twitter plugin for wordpress
 		Author: Susanto BSc (Xhanch Studio)
 		Author URI: http://xhanch.com
-		Version: 1.6.0
+		Version: 1.6.1
 	*/
 
 	define('xhanch_my_twitter', true);
--- xhanch_my_twitter_header_style.php
+++ xhanch_my_twitter_header_style.php
@@ -2,6 +2,8 @@
 	function xhanch_my_twitter_header_style(){		
 		$header_style = get_option('xhanch_my_twitter_header_style');
 		$username = get_option('xhanch_my_twitter_id');
+		$open_link_in_new_window = intval(get_option('xhanch_my_twitter_open_link_in_new_window'));
+
 		$twitter_url = 'http://twitter.com/'.$username;
 		$img_url = xhanch_my_twitter_get_dir('url').'/img/';
 
@@ -13,25 +15,25 @@
 			case '':
 				break;
 			case 'bird_with_text':
-				echo '<div class="header_48"><a href="'.$twitter_url.'" target="_blank"><img src="'.$img_url.'twitter-bird-'.$sty_var.'.png" class="img_left" alt="'.$username.'"/></a><a target="_blank" class="header_48 text_18" href="'.$twitter_url.'">'.get_option("xhanch_my_twitter_name").'</a><div class="clear"></div></div>';
+				echo '<div class="header_48"><a href="'.$twitter_url.'" '.($open_link_in_new_window?'target="_blank"':'').'><img src="'.$img_url.'twitter-bird-'.$sty_var.'.png" class="img_left" alt="'.$username.'"/></a><a '.($open_link_in_new_window?'target="_blank"':'').' class="header_48 text_18" href="'.$twitter_url.'">'.get_option("xhanch_my_twitter_name").'</a><div class="clear"></div></div>';
 				break;
 			case 'logo_with_text':
-				echo '<div class="header_48"><a href="'.$twitter_url.'" target="_blank"><img src="'.$img_url.'twitter-logo-'.$sty_var.'.png" class="img_left" alt="'.$username.'"/></a><a target="_blank" class="header_48 text_18" href="'.$twitter_url.'">'.get_option("xhanch_my_twitter_name").'</a><div class="clear"></div></div>';
+				echo '<div class="header_48"><a href="'.$twitter_url.'" '.($open_link_in_new_window?'target="_blank"':'').'><img src="'.$img_url.'twitter-logo-'.$sty_var.'.png" class="img_left" alt="'.$username.'"/></a><a '.($open_link_in_new_window?'target="_blank"':'').' class="header_48 text_18" href="'.$twitter_url.'">'.get_option("xhanch_my_twitter_name").'</a><div class="clear"></div></div>';
 				break;
 			case 'avatar':
 				$api_url_reply = 'http://twitter.com/users/'.urlencode($username).'.xml';
 				$req = xhanch_my_twitter_get_file($api_url_reply); 
 				if($req == ''){
-					echo '<div class="header_48"><a href="'.$twitter_url.'" target="_blank"><img src="'.$img_url.'twitter-bird-1.png" class="img_left" alt="'.$username.'"/></a><a target="_blank" class="header_48 text_18" href="'.$twitter_url.'">'.get_option("xhanch_my_twitter_name").'</a><div class="clear"></div></div>';
+					echo '<div class="header_48"><a href="'.$twitter_url.'" '.($open_link_in_new_window?'target="_blank"':'').'><img src="'.$img_url.'twitter-bird-1.png" class="img_left" alt="'.$username.'"/></a><a '.($open_link_in_new_window?'target="_blank"':'').' class="header_48 text_18" href="'.$twitter_url.'">'.get_option("xhanch_my_twitter_name").'</a><div class="clear"></div></div>';
 				}else{
 					$xml = @new SimpleXMLElement($req);
 					$img_url = $xml->profile_image_url;
 
-					echo '<div class="header_48"><a href="'.$twitter_url.'" target="_blank"><img src="'.$img_url.'" class="img_left" alt="'.$username.'"/></a><a target="_blank" class="header_48 text_18" href="'.$twitter_url.'">'.get_option("xhanch_my_twitter_name").'</a><div class="clear"></div></div>';
+					echo '<div class="header_48"><a href="'.$twitter_url.'" '.($open_link_in_new_window?'target="_blank"':'').'><img src="'.$img_url.'" class="img_left" alt="'.$username.'"/></a><a '.($open_link_in_new_window?'target="_blank"':'').' class="header_48 text_18" href="'.$twitter_url.'">'.get_option("xhanch_my_twitter_name").'</a><div class="clear"></div></div>';
 				}
 				break;
 			default:
-				echo '<div class="header_48"><a href="'.$twitter_url.'" target="_blank"><img src="'.$img_url.'twitter-bird-1.png" class="img_left" alt="'.$username.'"/></a><a target="_blank" class="header_48 text_18" href="'.$twitter_url.'">'.get_option("xhanch_my_twitter_name").'</a><div class="clear"></div></div>';
+				echo '<div class="header_48"><a href="'.$twitter_url.'" '.($open_link_in_new_window?'target="_blank"':'').'><img src="'.$img_url.'twitter-bird-1.png" class="img_left" alt="'.$username.'"/></a><a '.($open_link_in_new_window?'target="_blank"':'').' class="header_48 text_18" href="'.$twitter_url.'">'.get_option("xhanch_my_twitter_name").'</a><div class="clear"></div></div>';
 				break;
 		}
 	}
