343158505f36874b1e63f77de99dc76785ccfac0
xhanch_studio <xhanch_studio@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-10 13:57:22 +0000

    git-svn-id: https://plugins.svn.wordpress.org/xhanch-my-twitter/trunk@310161 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- xhanch-my-twitter.php
+++ xhanch-my-twitter.php
@@ -5,7 +5,7 @@
 		Description: Twitter plugin for wordpress
 		Author: Susanto BSc (Xhanch Studio)
 		Author URI: http://xhanch.com
-		Version: 2.3.5
+		Version: 2.3.6
 	*/
 	
 	define('xmt', true);
